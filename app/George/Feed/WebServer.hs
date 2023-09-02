66102cbd9fff0cea627bbe5af5738b519c34eeed
George Thomas <georgefsthomas@gmail.com>
2023-09-02 15:22:09 +0100

    Use utility in server stream

--- app/George/Feed/WebServer.hs
+++ app/George/Feed/WebServer.hs
@@ -1,6 +1,7 @@
 module George.Feed.WebServer (feed) where
 
 import George.Core
+import Util
 
 import Control.Concurrent
 import Control.Monad.Except
@@ -14,7 +15,6 @@ import Okapi hiding (Event, error, get, head)
 import Okapi qualified hiding (head)
 import Options.Generic
 import Streamly.Data.Stream.Prelude qualified as S
-import Streamly.Internal.Data.Stream.StreamK qualified as SK
 
 -- we only use this for actions which return a response
 server :: (forall m. (MonadIO m) => Event -> m ()) -> Wai.Application
@@ -32,21 +32,18 @@ server f =
         okPlainText [] . (<> "\n") . show' =<< liftIO (takeMVar m)
 
 feed :: (S.MonadAsync m) => Warp.Port -> S.Stream m [Event]
-feed port = SK.toStream $ SK.unCross do
-    m <- liftIO newEmptyMVar
-    -- TODO is there a better way to encapsulate getting initial mvar in to a stream than all this faffing with `SK`?
-    SK.mkCross . SK.fromStream . S.catMaybes $
-        S.parList
-            id
+feed port =
+    streamWithInit (liftIO newEmptyMVar) \m ->
+        (S.catMaybes . S.parList id)
             [ S.fromEffect $
                 liftIO $
                     (\() -> Nothing)
                         <$> Warp.runSettings
                             ( Warp.setLogger
-                                (curry3 $ unless . statusIsSuccessful . snd3 <*> putMVar m . ErrorEvent . Error "HTTP error")
+                                (curry3 $ unless . statusIsSuccessful . snd3 <*> putMVar m . pure . ErrorEvent . Error "HTTP error")
                                 . Warp.setPort port
                                 $ Warp.defaultSettings
                             )
-                            (server $ liftIO . putMVar m)
-            , S.repeatM $ Just . pure <$> liftIO (takeMVar m)
+                            (server $ liftIO . putMVar m . pure)
+            , S.repeatM $ Just <$> liftIO (takeMVar m)
             ]
