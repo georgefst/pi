f9678849311ef79654bc6367bb8c2d66a5075439
George Thomas <georgefsthomas@gmail.com>
2023-09-08 01:17:51 +0100

    Formatting

--- app/George/Feed/Keyboard.hs
+++ app/George/Feed/Keyboard.hs
@@ -241,15 +241,14 @@ dispatchKeys opts = wrap \case
         ref <- liftIO $ newIORef True
         #typing ?= (t, [], writeIORef ref False)
         mode <- use #mode
-        tell . pure . S.cons [LogEvent "Waiting for keyboard input"] $
-            case opts.modeLED mode of
-                Just led ->
-                    S.takeWhileM (const $ liftIO $ readIORef ref) . S.concatMap id . S.repeat $
-                        (S.consM pause . S.cons [setLED False] . S.consM pause . S.cons [setLED True] $ S.nil)
-                  where
-                    pause = liftIO (threadDelay 300_000) >> pure []
-                    setLED = ActionEvent mempty . send . SetLED led
-                Nothing -> S.nil
+        tell . pure $ S.cons [LogEvent "Waiting for keyboard input"] case opts.modeLED mode of
+            Just led ->
+                S.takeWhileM (const $ liftIO $ readIORef ref) . S.concatMap id . S.repeat $
+                    (S.consM pause . S.cons [setLED False] . S.consM pause . S.cons [setLED True] $ S.nil)
+              where
+                pause = liftIO (threadDelay 300_000) >> pure []
+                setLED = ActionEvent mempty . send . SetLED led
+            Nothing -> S.nil
     speakerName = "pi"
 
 feed :: (S.MonadAsync m, MonadLog Text m) => [Text] -> Mode -> Opts -> S.Stream m [Event]
